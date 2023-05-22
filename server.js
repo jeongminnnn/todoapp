d1bf88627a48e34059de7fda72ff420697096336
현정민 <hyeonjeongmin@hyeonjeongmin-ui-MacBookAir.local>
2023-05-23 07:58:06 +0900

    게시물 번호 부여

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const app = express()
-const MongoClient = require('mongodb').MongoClient
-let db
+
 app.use(express.urlencoded({extended: true}))
 app.set('view engine', 'ejs')
+
+
+app.get('/write', (req, res) => res.sendFile(__dirname + '/write.html'))
+
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'))
+
+const MongoClient = require('mongodb').MongoClient
+let db
 MongoClient.connect('mongodb+srv://admin:qwer1234@cluster0.0p5tziw.mongodb.net/todoapp?retryWrites=true&w=majority', { useUnifiedTopology: true }, function(에러, client){
 
     // 연결되면 할 일
@@ -15,27 +22,34 @@ MongoClient.connect('mongodb+srv://admin:qwer1234@cluster0.0p5tziw.mongodb.net/t
         console.log('저장완료')
     })
 
-    app.listen(8080, function () {
-        console.log('listening on 8080')
-    })
-
-    app.get('/write', function(req, res) {
-
-        res.sendFile(__dirname + '/write.html')
-    })
-
-    app.get('/', function(req, res) {
-
-        res.sendFile(__dirname + '/index.html')
-    })
 
     app.post('/add', (req, res) => {
 
         res.send('전송완료')
-        db.collection('post').insertOne({제목: req.body.title, 날짜: req.body.date}, function(에러, 결과) {
-            console.log('저장완료')
+
+        db.collection('counter').findOne({ name:'게시물갯수' }, (err, result) => {
+
+            let 총게시물갯수 = result.totalPost
+
+            db.collection('post').insertOne({ _id: 총게시물갯수+1, 제목: req.body.title, 날짜: req.body.date}, (err, result) => {
+                console.log('저장완료')
+
+                db.collection('counter').updateOne({ name:'게시물갯수' }, { $inc : {totalPost:1} }, (err, result) => {
+                    if (err) {return console.log(err)}
+                })
+            })
         })
     })
+
+    app.get('/list', (req, res) => {
+
+        db.collection('post').find().toArray((err, result) => {
+            console.log(result)
+            res.render('list.ejs', { posts : result })
+        })
+    })
+
+    app.listen(8080, () => console.log('listening on 8080'))
 })
 
 
